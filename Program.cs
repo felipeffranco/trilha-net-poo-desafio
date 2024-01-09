0961f5fc6e6d36ca9f3f540f2914c577cd41dc80
felipeffranco <felipeffranco@outlook.com>
2024-01-09 11:45:49 -0300

    projeto concluido com sucesso

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia");
+Smartphone nokia = new Nokia(numero: "(11) 99545-3112", modelo: "Nokia G60", imei: "354948230", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Notion");
+
+Console.WriteLine("Iphone");
+Smartphone iphone = new Iphone(numero: "(11) 99545-3112", modelo: "Iphone 15 Pro Max", imei: "39210912", memoria: 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Github");
